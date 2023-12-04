5fb23e68353225fd3b598cf3684a22403f43f4b5
Black-ceaser <calvin.annan@gmail.com>
2023-12-04 09:32:28 -0800

    making progress

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_HEADER_FILE
+#define SHELL_HEADER_FILE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+
+
+#endif /*SHELL_HEADER_FILE*/
