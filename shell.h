a4a0228abfa76a30d4211556783fdc806f2f7dbb
Black-ceaser <calvin.annan@gmail.com>
2023-12-12 09:11:49 -0800

    updated

--- shell.h
+++ shell.h
@@ -17,6 +17,95 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
+/**
+ * struct liststr - linked list
+ * @numb: the number of fields
+ * @str: string
+ * @next: points to the next node
+ */
+typedef struct liststr
+{
+	int numb;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct passinfo - contains pseudo-arguments to pass into a function
+ * @arg: string generated from geline
+ * @argv: array of stingss generated
+ * @path: string path for the current command
+ * @argc: argument count
+ * @line_count: error count
+ * @err_num: error code for exit()s
+ * @linecount_flag: count this line of input
+ * @fname: program filename
+ * @env: linked list
+ * @environ: custom modified copy of environ
+ * @history: history node
+ * @alias: alias node
+ * @env_changed: environ was changed
+ * @status: last command of the executed command
+ * @cmd_buf: address of pointer to cmd_buf
+ * @cmd_buf_type: CMD_type ||, && ;
+ * @readfd: the fd from which to read line input
+ * @histcount: history line count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	int err_num;
+	unsigned int line_count;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf;
+	int cmd_buf_type;
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/**
+ * struct builtin - contain builtin string and functions
+ * @type: builtin command flag
+ * @func: function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func) (info_t *);
+} builtin_table;
+
+char **strtow(char *, char *);
 
 
 #endif /*SHELL_HEADER_FILE*/
